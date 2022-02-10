3a2c6e9cd90e71e7aa6eecb80d3721b9e7df1b43
Josh Burns <83684810+joshburnsxyz@users.noreply.github.com>
2022-02-10 13:41:35 +1100

    register headers for all color functions

--- lib/DisplayModes/DisplayPatterns.h
+++ lib/DisplayModes/DisplayPatterns.h
@@ -4,5 +4,7 @@ class DisplayPatterns
 {
     public:
         void StaticGlowBlue(CRGB leds, int num_leds);
+        void StaticGlowGreen(CRGB leds, int num_leds);
+        void StaticGlowRed(CRGB leds, int num_leds);
         void Off(CRGB leds, int num_leds);
 }
