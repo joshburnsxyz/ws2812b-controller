d67b2a142c886d7385324fe07eb5896d18dc2346
Josh Burns <83684810+joshburnsxyz@users.noreply.github.com>
2022-02-10 13:57:22 +1100

    use integer to track current mode, increment on button press

--- src/main.cpp
+++ src/main.cpp
@@ -2,26 +2,43 @@
 #include <DisplayPatterns.h>
 
 #define LED_PIN     7
+#define BTN_PIN     4
 #define NUM_LEDS    20
-#define DISP_MODE   0
 
+// LED Interface
 CRGB leds[NUM_LEDS];
 
+// Default button state
+int btnState = 0;
+
+// Default mode
+int dispMode = 0;
+
 void setup() {
-  FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);  
+  FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
+  pinMode(BTN_PIN, INPUT);
 }
 
 void loop() {
-    switch(DISP_MODE) {
+    // read button state
+    if (btnState == HIGH) {
+        dispMode = dispMode + 1;
+        if (dispMode > 3) {
+            dispMode = 0;
+        }
+    }
+    switch(dispMode) {
         case 0:
             // Blue
             DisplayPatterns::StaticGlowBlue(leds, NUM_LEDS);
             break;
         case 1:
-            // Demo 1
+            // Green
+            DisplayPatterns::StaticGlowGreen(leds, NUM_LEDS);
             break;
         case 2:
-            // Demo 2
+            // Red
+            DisplayPatterns::StaticGlowRed(leds, NUM_LEDS);
             break;
         case 3:
             // Off
