9292e4f6ea26a47ab46c815ff84a2c8f15a2c94b
Josh Burns <83684810+joshburnsxyz@users.noreply.github.com>
2022-02-10 13:12:05 +1100

    add a half-second delay between LEDs turning on

--- lib/DisplayModes/DisplayPatterns.cpp
+++ lib/DisplayModes/DisplayPatterns.cpp
@@ -2,5 +2,7 @@ void DisplayPatterns::StaticGlow(CRGB leds, int num_leds)
     for (int i = 0; i <= num_leds; i++) {
         leds[i] = CRGB(36, 190, 255); // Hard-Coded color value
         FastLED.show();
+        delay(500);
     }
+    return;
 }
